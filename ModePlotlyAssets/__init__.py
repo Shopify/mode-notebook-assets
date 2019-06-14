682b463e9e70a463a666dc0d01485563dac0a6bf
Vincent Berube <vincent.berube@shopify.com>
2019-06-14 10:04:04 -0400

    cleanup

--- ModePlotlyAssets/__init__.py
+++ ModePlotlyAssets/__init__.py
@@ -1,7 +1,1 @@
-import plotly
-import plotly.graph_objs as go
-import plotly.figure_factory as ff
-from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
-from plotly import tools #subplot
-init_notebook_mode(connected=False)
 
