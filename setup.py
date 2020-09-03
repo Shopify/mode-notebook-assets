f39b42ae3d57069e77f3f9db74e72e884eb3d905
Vincent Berube <vincent.berube@shopify.com>
2020-09-03 19:56:41 -0400

    module rename & move

--- setup.py
+++ setup.py
@@ -12,24 +12,11 @@ else:
         long_description = f.read()
 
 version = {}
-with open(os.path.join(_here, 'ModeNotebookAssets', 'version.py')) as f:
+with open(os.path.join(_here, 'mode_notebook_assets', 'version.py')) as f:
     exec(f.read(), version)
 
-def list_subpackages(package_name):
-    yield package_name
-
-    root = os.path.dirname(os.path.realpath(__file__))
-
-    parent_directory = os.path.join(root, *package_name.split('.'))
-
-    for dir in os.listdir(parent_directory):
-        path = os.path.join(parent_directory, dir)
-        if os.path.isdir(path) and os.path.exists(os.path.join(path, '__init__.py')):
-            for i in list_subpackages(package_name + '.' + dir):
-                yield i
-
 setup(
-    name='ModeNotebookAssets',
+    name='mode_notebook_assets',
     version=version['__version__'],
     description=('Convenience Classes for working with Notebooks in ModeAnalytics'),
     long_description=long_description,
@@ -37,21 +24,15 @@ setup(
     author_email='',
     url='https://github.com/Shopify/mode-notebook-assets',
     license='MPL-2.0',
-    packages=['ModeNotebookAssets', 'ModeNotebookAssets.bignum'],
-    # packages=list(list_subpackages('ModeNotebookAssets')),
-    # package_dir={'bignum': 'ModeNotebookAssets', 'ModeNotebookAssets': 'ModeNotebookAssets'},
-    # packages=find_packages(include=["ModeNotebookAssets", "ModeNotebookAssets.bignum", "ModeNotebookAssets.table"]),
-    # package_dir={'ModeNotebookAssets': 'ModeNotebookAssets', 'bignum': 'bignum', 'table': 'table'},
-#   no dependencies in this example
+    packages=['mode_notebook_assets'],
 #   install_requires=[
 #       'dependency==1.2.3',
 #   ],
-#   no scripts in this example
 #   scripts=['bin/a-script'],
     include_package_data=True,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
-        # 'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6'],
+        'Programming Language :: Python :: 3.6'
+        ],
     )
