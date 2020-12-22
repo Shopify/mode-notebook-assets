6b2bb39e45ed5eddd8640d3b6a130439dce9af69
Joel Becker <joel.becker@shopify.com>
2020-12-22 18:45:00 -0500

    adds option to limit rows in display table

--- mode_notebook_assets/practical_dashboard_displays.py
+++ mode_notebook_assets/practical_dashboard_displays.py
@@ -719,7 +719,7 @@ def dot(color='gray', figsize=(.5, .5), **kwargs):
 
 def convert_metric_status_table_to_html(df: pd.DataFrame, title=None, include_actionability_score=False,
                                         sort_records_by_actionability=False, sort_records_by_value=False,
-                                        font_color='#3C3C3C', title_color='#2A3F5F'):
+                                        limit_rows: int = None, font_color='#3C3C3C', title_color='#2A3F5F'):
 
     _df = df.copy()
 
@@ -742,6 +742,9 @@ def convert_metric_status_table_to_html(df: pd.DataFrame, title=None, include_ac
 
     if include_actionability_score is False and 'Actionability Score' in _df.columns:
         _df = _df[[c for c in _df.columns if c != 'Actionability Score']]
+
+    if limit_rows is not None:
+        _df = _df.head(limit_rows)
 
     _output = _df.style.set_table_styles(
         [{'selector': '*',
