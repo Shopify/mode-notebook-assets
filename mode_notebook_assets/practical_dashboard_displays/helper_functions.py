20ffa3664b3d2d0e03f2be5214cc2ed47f2a1ca0
Joel Becker <joel.becker@shopify.com>
2021-03-24 14:31:25 -0400

    adds actionability score normalization helper function

--- mode_notebook_assets/practical_dashboard_displays/helper_functions.py
+++ mode_notebook_assets/practical_dashboard_displays/helper_functions.py
@@ -1,6 +1,40 @@
+import numpy as np
+
+
 def functional_setattr(__obj, __name, __value):
     """
-    Same as builting setattr but returns __obj instead of None.
+    Same as builtin setattr but returns __obj instead of None.
     """
     setattr(__obj, __name, __value)
     return __obj
+
+
+def normalize_actionability_score(_raw_score: float, is_higher_better: bool, is_lower_better: bool):
+    """
+    Normalize a raw actionability score. Normalized actionability scores are floats between -1 and 1
+    where the sign of the score represents the valence (i.e. positive is good and negative is bad).
+    Scores whose absolute value is bigger than 1 are truncated because larger absolute values are
+    not meaningful in the Practical Dashboards 4-threshold valence score system.
+
+    Parameters
+    ----------
+    _raw_score: float
+    is_higher_better: bool
+    is_lower_better: bool
+
+    Returns
+    -------
+    float between -1 and 1
+    """
+    if is_higher_better and not is_lower_better:
+        _normalized_score = _raw_score
+    elif is_higher_better and not is_lower_better:
+        _normalized_score = _raw_score * -1
+    elif not is_higher_better and not is_lower_better:
+        _normalized_score = np.abs(_raw_score) * -1
+    else:
+        _normalized_score = np.abs(_raw_score)
+
+    _truncated_score = np.ceil([np.floor([_normalized_score, -1]), 1])
+
+    return _truncated_score
