5c7bb0b3d334110c24aede3cfa86508c86fbd49c
Joel Becker <joel.becker@shopify.com>
2021-03-24 16:38:53 -0400

    adds draft tests for metric checks

---
+++ test/practical_dashboard_displays/metric_evaluation_pipeline/metric_checks/test_manual_four_threshold_metric_check.py
@@ -0,0 +1,36 @@
+from itertools import permutations
+
+import pytest
+import pandas as pd
+
+from mode_notebook_assets.practical_dashboard_displays.metric_evaluation_pipeline.metric_checks.manual_four_threshold_metric_check import \
+    ManualFourThresholdMetricCheck
+
+TEST_CONFIGURATION = ManualFourThresholdMetricCheck(
+    threshold_1=10,
+    threshold_2=20,
+    threshold_3=30,
+    threshold_4=40,
+)
+
+
+def test_init_manual_four_threshold_metric_check():
+    assert TEST_CONFIGURATION, 'Object should initialize.'
+
+
+def test_invalid_configuration():
+    # Initialization should fail if the thresholds
+    # aren't in sorted order.
+    with pytest.raises(AssertionError):
+        for _set in permutations([10, 20, 30, 40], r=4):
+            if _set != _set.sort():
+                assert ManualFourThresholdMetricCheck(
+                    threshold_1=_set[0],
+                    threshold_2=_set[1],
+                    threshold_3=_set[2],
+                    threshold_4=_set[3],
+                )
+
+
+def test_run_check():
+    assert TEST_CONFIGURATION.run(pd.Series([25, 25, 5, 15, 35, 45])), 'Run method should finish executing.'
