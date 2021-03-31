845292a37714cfcbce1b4dbf4aaff4fbc82b78fd
Joel Becker <joel.becker@shopify.com>
2021-03-31 13:27:44 -0400

    switches matplotlib backend to solve tkinter import problem

--- mode_notebook_assets/practical_dashboard_displays/legacy_helper_functions.py
+++ mode_notebook_assets/practical_dashboard_displays/legacy_helper_functions.py
@@ -3,6 +3,9 @@ from io import BytesIO
 
 import numpy as np
 import pandas as pd
+import matplotlib
+
+matplotlib.use('agg')
 from matplotlib import pyplot as plt
 from plotly import express as px
 
