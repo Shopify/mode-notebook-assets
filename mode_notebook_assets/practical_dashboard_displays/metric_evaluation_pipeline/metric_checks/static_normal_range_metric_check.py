03a7a314ada67ddd73dcaca85b4a793550cdfe34
Joel Becker <joel.becker@shopify.com>
2021-03-31 17:07:48 -0400

    draft of metric check and test

---
+++ mode_notebook_assets/practical_dashboard_displays/metric_evaluation_pipeline/metric_checks/static_normal_range_metric_check.py
@@ -0,0 +1,147 @@
+from dataclasses import dataclass
+from typing import Iterable
+
+import pandas as pd
+
+from mode_notebook_assets.practical_dashboard_displays.helper_functions import normalize_valence_score, \
+    map_score_to_string, map_sign_to_string
+from mode_notebook_assets.practical_dashboard_displays.metric_evaluation_pipeline.metric_check_results \
+    import MetricCheckResult
+from mode_notebook_assets.practical_dashboard_displays.metric_evaluation_pipeline.metric_checks.abstract_metric_check \
+    import AbstractMetricCheck
+
+
+@dataclass
+class StaticNormalRangeMetricCheck(AbstractMetricCheck):
+    """
+    This class implements the static "within a normal range" metric check.
+    This check assumes that the metric is more or less static within
+    the training window. For use on metric that have a long term trend,
+    use the "dynamic normal range" metric check.
+    """
+    is_higher_better: bool = True
+    is_lower_better: bool = False
+    is_rolling_window: bool = True
+    rolling_periods: int = 12
+    l1_normal_range_constant: float = 2.66
+    l2_normal_range_constant: float = 3.99
+    maximum_learning_differences_quantile: float = 1
+
+    # "Private" class attributes
+    _metric_check_label: str = 'Static Normal Range Check'
+
+    def _calculate_central_measure(self, s: pd.Series) -> pd.Series:
+        """
+        Calculate a central measure e.g. rolling average or a linear trend.
+        In this case I'm using a rolling average as a default.
+        """
+        if self.is_rolling_window:
+            return s.rolling(self.rolling_periods).mean()
+        else:
+            return s.expanding().mean()
+
+    def _calculate_mean_of_period_over_period_differences(self, s: pd.Series) -> pd.Series:
+        """
+        Calculate the period over period differences for use
+        in statistical process control calculations.
+        """
+        def sum_under_threshold(window: Iterable) -> float:
+            return sum(x for x in window if x <= _maximum_learning_value)
+
+        _differences_series = abs(s - s.shift(1))
+        _maximum_learning_value = _differences_series.quantile(self.maximum_learning_differences_quantile)
+
+        if self.is_rolling_window:
+            return _differences_series.rolling(self.rolling_periods).apply(sum_under_threshold)
+        else:
+            return _differences_series.expanding().apply(sum_under_threshold)
+
+    def _calculate_thresholds_and_scores(self, s: pd.Series, central_measure: pd.Series,
+                                         mean_differences: pd.Series) -> pd.DataFrame:
+        """
+        Calculates thresholds and raw valence scores.
+        """
+        def _calculate_threshold_series(sign: int, constant: float) -> pd.Series:
+            return central_measure + (sign * constant * mean_differences)
+
+        def _calculate_raw_valence_score(record: dict) -> float:
+            if record['period_value'] <= record['lower_l2_threshold']:
+                return -1
+            elif record['period_value'] <= record['lower_l1_threshold']:
+                return (
+                    (record['period_value'] - record['lower_l1_threshold']) /
+                    (record['lower_l1_threshold'] - record['lower_l2_threshold'])
+                )
+            elif record['period_value'] >= record['higher_l2_threshold']:
+                return 1
+            elif record['period_value'] >= record['higher_l1_threshold']:
+                return (
+                        (record['period_value'] - record['higher_l1_threshold']) /
+                        (record['higher_l2_threshold'] - record['higher_l1_threshold'])
+                )
+            else:
+                assert record['higher_l1_threshold'] > record['period_value'] > record['lower_l1_threshold'], \
+                    'Check that threshold logic is correct.'
+                return 0
+
+        _threshold_df = pd.DataFrame(central_measure).assign(
+            period_value=s,
+            lower_l2_threshold=_calculate_threshold_series(-1, self.l2_normal_range_constant),
+            lower_l1_threshold=_calculate_threshold_series(-1, self.l1_normal_range_constant),
+            higher_l1_threshold=_calculate_threshold_series(1, self.l1_normal_range_constant),
+            higher_l2_threshold=_calculate_threshold_series(1, self.l2_normal_range_constant),
+        )
+
+        _raw_scores = _threshold_df.apply(_calculate_raw_valence_score)
+
+        return _threshold_df.assign(raw_valence_scores=_raw_scores)
+
+    def run(self, s: pd.Series) -> pd.Series:
+        """
+        Run method for the normal range metric check.
+
+        Parameters
+        ----------
+        s: metric time series
+
+        Returns
+        -------
+        Series of MetricCheckResults
+        """
+        def _map_raw_score_to_result(_raw_score: float) -> MetricCheckResult:
+
+            _normalized_score = normalize_valence_score(
+                _raw_score=_raw_score,
+                is_higher_better=self.is_higher_better,
+                is_lower_better=self.is_lower_better
+            )
+
+            _base_description = map_sign_to_string(_raw_score, [
+                    'Lower than normal based on historical values.',
+                    'Within the range of normal based on historical values.',
+                    'Higher than normal based on historical values.'
+                ],
+                                                   )
+
+            return MetricCheckResult(
+                valence_score=_normalized_score,
+                valence_label=map_score_to_string(_normalized_score),
+                valence_description=map_score_to_string(_normalized_score, labels=[
+                    f'Significantly {_base_description.lower()}',
+                    _base_description,
+                    _base_description,
+                    _base_description,
+                    f'Significantly {_base_description.lower()}',
+                ]),
+                metric_check_label=self._metric_check_label,
+            )
+
+        _central_measure_series = self._calculate_central_measure(s)
+        _mean_period_over_period_difference_series = self._calculate_mean_of_period_over_period_differences(s)
+        _score_and_threshold_dataframe = self._calculate_thresholds_and_scores(
+            s=s,
+            central_measure=_central_measure_series,
+            mean_differences=_mean_period_over_period_difference_series,
+        )
+
+        return _score_and_threshold_dataframe.raw_valence_scores.apply(_map_raw_score_to_result)
---
+++ test/practical_dashboard_displays/metric_evaluation_pipeline/metric_checks/test_static_normal_range_metric_check.py
@@ -0,0 +1,20 @@
+from itertools import permutations
+
+import pytest
+import pandas as pd
+
+from mode_notebook_assets.practical_dashboard_displays.metric_evaluation_pipeline.\
+    metric_check_results import MetricCheckResult
+from mode_notebook_assets.practical_dashboard_displays.metric_evaluation_pipeline.metric_checks.\
+    static_normal_range_metric_check import StaticNormalRangeMetricCheck
+
+TEST_CONFIGURATION = StaticNormalRangeMetricCheck()
+
+
+def test_init_manual_four_threshold_metric_check():
+    assert TEST_CONFIGURATION, 'Object should initialize.'
+
+
+def test_run_check():
+    assert isinstance(TEST_CONFIGURATION.run(pd.Series([25, 25, 5, 15, 35, 45])), pd.Series), \
+        'Run method should finish executing.'
