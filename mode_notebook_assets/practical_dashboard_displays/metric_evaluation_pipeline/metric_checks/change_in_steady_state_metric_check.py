58bfbb38bf23ea498623d975d2749e9a2415dbbe
Allison Hill <allison.hill@shopify.com>
2021-03-31 20:20:42 -0400

    neatened up logic/functions for actionability score

--- mode_notebook_assets/practical_dashboard_displays/metric_evaluation_pipeline/metric_checks/change_in_steady_state_metric_check.py
+++ mode_notebook_assets/practical_dashboard_displays/metric_evaluation_pipeline/metric_checks/change_in_steady_state_metric_check.py
@@ -24,7 +24,7 @@ class ChangeInSteadyStateMetricCheck(AbstractMetricCheck):
     longest_run_1_threshold: int = 7
     longest_run_2_threshold: int = 9
 
-    _min_periods = 2
+    _min_periods = 1
 
     def run(self, s: pd.Series) -> pd.Series:
         # ------ PSEUDO CODE ------
@@ -43,49 +43,33 @@ class ChangeInSteadyStateMetricCheck(AbstractMetricCheck):
 
 
         # Calculate rolling & expanding averages, and store
-        tmp = tmp.assign(
-                    values = s,
-                    rolling_average = s.rolling(rolling_window_size).mean(),
-                    sign = np.sign(s - s.rolling(rolling_window_size).mean()),
-                    # get sign of the difference + or - 1
-                    # sign_series.rolling(L1_THRESHOLD).apply() <- are ALL values in this window same sign?
-                    # sign_series.rolling(L2_THRESHOLD).apply() <- are ALL values in this window same sign?
-                    # score is +/- 0.1 based on the l1 series
-                    #
+        df = pd.DataFrame(
+                {   'metric':           s,
+                    'rolling_average':  s.rolling(rolling_window_size, min_periods=_min_periods).mean(),
+                    'sign':             np.sign(s - s.rolling(rolling_window_size, min_periods=_min_periods).mean())
+                }
+            )
 
-                    # expanding_average = s.expanding(min_periods=_min_periods).mean(),
-         )
+        # calculate the rolling sums with windows equal in size to our L1 & L2 thresholds
+        df = df.assign(
+                rolling_sum_L1_THRESHOLD = df.sign.rolling(L1_THRESHOLD, min_periods=1).sum(),
+                rolling_sum_L2_THRESHOLD = df.sign.rolling(L2_THRESHOLD, min_periods=1).sum(),
+                activation = 0
+            )
 
-         tmp = tmp.assign(
-                    rolling_sum_L1_THRESHOLD = tmp.sign.rolling(L1_THRESHOLD).sum(),
-                    rolling_sum_L2_THRESHOLD = tmp.sign.rolling(L2_THRESHOLD).sum(),
-                    activation = 0
-         )
+        # define mapping to actionability score
+        def _actionability_score_mapping(r, L1_THRESHOLD, L2_THRESHOLD):
+            '''
+            This method takes the rolling count above/below the L1/L2 thresholds and maps
+            them to an actionability score
+            '''
+            if abs(r.rolling_sum_L2_THRESHOLD) >= L2_THRESHOLD:
+                return np.sign(r.rolling_sum_L2_THRESHOLD)
+            else:
+                return np.sign(r.rolling_sum_L1_THRESHOLD) * (((abs(r.rolling_sum_L1_THRESHOLD) - L1_THRESHOLD) / L2_THRESHOLD) + 0.01)
 
-         tmp.activation.loc[tmp.rolling_sum_L2 >= L2_THRESHOLD] = 1
-         tmp.activation.loc[tmp.rolling_sum_L2 <= L2_THRESHOLD] = -1
-         tmp.activation.loc[(tmp.rolling_sum_L1 >= L1_THRESHOLD) and (tmp.rolling_sum_L1 <= L2_THRESHOLD)] = 0.01 + (tmp.rolling_sum_L1 - L1_THRESHOLD)/(L2_THRESHOLD - L1_THRESHOLD)   #prob a cleverer way of getting +ve, -ve cases
-         tmp.activation.loc[(tmp.rolling_sum_L1 >= -L1_THRESHOLD) and (tmp.rolling_sum_L1 <= -L2_THRESHOLD)] = -1*(0.01 + (tmp.rolling_sum_L1 - L1_THRESHOLD)/(L2_THRESHOLD - L1_THRESHOLD))
+        # apply mapping
+        df['activation'] = df.apply(lambda row: _actionability_score_mapping(row), axis=1)
 
-
-        # determine the length of the longest run
-        flip_signs = np.sign(tmp['value'] - tmp['rolling_average'])  # is it above or below average?
-
-        flip_locations = np.where(np.diff(flip_signs))[0]
-        flip_locations = np.insert(flip_locations, 0, 0)    # put in first index
-        flip_locations = np.append(len(tmp['value']) - 1)   # put in last index
-
-        length_of_runs = np.diff(flip_locations)
-
-        # pseudo-ish
-        # find out name of actionable whatever later, but we want to do mapping
-        # actionable = length_of_runs.any() > longest_run_1_threshold
-        # super_actionable = length_of_runs.any()  > longest_run_2_threshold
-
-
-
-        # this will have leading nulls of size s.len - window_size
-        # expanding_mean = s.expanding().mean()
-        # rolling_mean = s.rolling(rolling_window_size, min_periods = None, win_type = _win_type, closed=None).mean()
         pass
         # return _output
