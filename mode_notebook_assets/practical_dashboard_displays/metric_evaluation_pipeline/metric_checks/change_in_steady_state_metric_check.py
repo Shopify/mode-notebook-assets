3343c23494279bb0a036e7eb28770be2fd2f21b6
Iris Lieuw <iris.lieuw@shopify.com>
2021-04-01 10:15:59 -0700

    Add ChangeInSteadyStateMetricCheck tests and remaining logic

--- mode_notebook_assets/practical_dashboard_displays/metric_evaluation_pipeline/metric_checks/change_in_steady_state_metric_check.py
+++ mode_notebook_assets/practical_dashboard_displays/metric_evaluation_pipeline/metric_checks/change_in_steady_state_metric_check.py
@@ -19,19 +19,20 @@ class ChangeInSteadyStateMetricCheck(AbstractMetricCheck):
 
     # for checks with running averages
     is_rolling_window: bool = True # if false, use an expanding window
+    rolling_periods: int = 7
 
     # define longest run thresholds
     l1_threshold: int = 7
     l2_threshold: int = 9
 
-    _rolling_periods: int = longest_run_1_threshold
-
     _min_periods = 1
+
+    def __post_init__(self):
+        assert self.l1_threshold < self.l2_threshold, 'L1 threshold must be less than L2 threshold.'
 
     def run(self, s: pd.Series) -> pd.Series:
         # ------ PSEUDO CODE ------
         # Validity checks (enough values in the series? valid number of rolling periods?)
-        # Split series into baseline / test
         # Calculate the mean on the baseline
         # Use 7 & 9 for longest run thresholds
             # a. Calculate default thresholds based on th mean (stdev or something else)
@@ -43,20 +44,37 @@ class ChangeInSteadyStateMetricCheck(AbstractMetricCheck):
         #  Normalize raw scores
         # ------ END PSEUDO CODE ------
 
+        self._validate_inputs(s)
 
-        # Calculate rolling & expanding averages, and store
+        # Calculate rolling or expanding mean, and store
+        metric_mean = s.rolling(self.rolling_periods, min_periods = self._min_periods).mean() if self.is_rolling_window else s.expanding(min_periods = self._min_periods).mean()
+
         df = pd.DataFrame(
-                {   'metric':  s,
-                    'sign':    np.sign(s - s.rolling(self._rolling_periods, min_periods = self._min_periods).mean())
-                }
-            )
+            {   'metric':  s,
+                'metric_mean': metric_mean,
+                'sign':    np.sign(s - metric_mean)
+            }
+        )
 
-        # calculate the rolling sums with windows equal in size to our L1 & L2 thresholds and make default activation 0
+        # Track longest runs
+        curr_val = None
+        curr_consecutive_count = 0
+        running_reps = []
+        for x in df.sign:
+            if curr_val is None:
+                curr_val = x
+            if x == curr_val:
+                curr_consecutive_count += x
+            else:
+                curr_val = x
+                curr_consecutive_count = x
+            running_reps.append(curr_consecutive_count)
+        df['running_reps'] = running_reps
+
+        # Make default activation 0
         df = df.assign(
-                rolling_sum_l1_threshold = df.sign.rolling(self.l1_threshold, min_periods = self._min_periods).sum(),
-                rolling_sum_l2_threshold = df.sign.rolling(self.l2_threshold, min_periods = self._min_periods).sum(),
-                activation = 0
-            )
+            activation = 0
+        )
 
         # define mapping to actionability score
         def _actionability_score_mapping(r, L1_THRESHOLD, L2_THRESHOLD):
@@ -64,12 +82,51 @@ class ChangeInSteadyStateMetricCheck(AbstractMetricCheck):
             This method takes the rolling count above/below the L1/L2 thresholds and maps
             them to an actionability score
             '''
-            if abs(r.rolling_sum_l2_threshold) >= L2_THRESHOLD:
-                return np.sign(r.rolling_sum_l2_threshold)
-            elif abs(r.rolling_sum_l2_threshold) < L1_THRESHOLD:
+            if abs(r.running_reps) >= L2_THRESHOLD:
+                return np.sign(r.running_reps)
+            elif abs(r.running_reps) < L1_THRESHOLD:
                 return 0
             else:
-                return np.sign(r.rolling_sum_l2_threshold) * (((abs(r.rolling_sum_l2_threshold) - L1_THRESHOLD) / (L2_THRESHOLD - L1_THRESHOLD)) + 0.01)
+                return np.sign(r.running_reps) * (((abs(r.running_reps) - L1_THRESHOLD) / (L2_THRESHOLD - L1_THRESHOLD)) + 0.01)
 
         # apply mapping and return
-        return df.apply(lambda row: _actionability_score_mapping(row), axis=1)
+        df.activation = df.apply(lambda row: _actionability_score_mapping(row, self.l1_threshold, self.l2_threshold), axis=1)
+
+        def map_value_to_result(_raw_score: float) -> MetricCheckResult:
+
+            _normalized_score = normalize_valence_score(
+                _raw_score,
+                is_higher_better=self.is_higher_better,
+                is_lower_better=self.is_lower_better,
+            )
+
+            _base_description = map_sign_to_string(
+                _raw_score, [
+                    'Drop in steady state below historical mean.',
+                    'No significant change from historical mean.',
+                    'Rise in steady state above historical mean.'
+                ],
+            )
+
+            # Maybe handle NaNs
+
+            return MetricCheckResult(
+                valence_score=_normalized_score,
+                valence_label=map_score_to_string(_normalized_score),
+                valence_description=map_score_to_string(_normalized_score, labels=[
+                    f'Significant {_base_description.lower()}',
+                    _base_description,
+                    _base_description,
+                    _base_description,
+                    f'Significant {_base_description.lower()}',
+                ]),
+                metric_check_label='Change in Steady State Check',
+            )
+
+        _output = df.activation.apply(
+            map_value_to_result
+        )
+
+        self._validate_output(s=s, _output=_output)
+
+        return _output
