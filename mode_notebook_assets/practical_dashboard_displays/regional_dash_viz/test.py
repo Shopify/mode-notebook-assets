4f60f9369c93249d395a1a973b76a49c259b7d0f
Radwan Allahoum <radwan.allahoum@shopify.com>
2021-04-01 15:55:13 -0400

    added changes

--- mode_notebook_assets/practical_dashboard_displays/regional_dash_viz/test.py
+++ mode_notebook_assets/practical_dashboard_displays/regional_dash_viz/test.py
@@ -1,16 +1,15 @@
-#%%
-
-import pandas_gbq as pbq
-
+# %%
 project_id = "shopify-data-bigquery-global"
-
-
+import pandas_gbq as pbq
 def test_query():
-    return '''select * from scratch.practical_dashboards_data'''
+    return '''select * from scratch.practical_dashboards_data
+    where metric = 'gmv' and segment = 'overall' and region = 'Canada'
+    order by snap_date asc
+    '''
 df = pbq.read_gbq(test_query(), project_id=project_id)
+df = df.set_index('snap_date')
 # %%
 from mode_notebook_assets.practical_dashboard_displays.metric_evaluation_pipeline.metric_evaluation_pipeline import MetricEvaluationPipeline
-# %%
 import pandas as pd
 import numpy as np
 import plotly as p
@@ -23,163 +22,125 @@ from plotly import tools
 
 # %%
 
+def get_cumulative_values(s: pd.Series):
+    return s.cumsum()
+
 @dataclass
-class BuildTargetPlots:
+class BuildTargetPlots():
     """
     Build 
     """
     actuals: pd.Series
     date_range: pd.Series
-    targets: pd.Series
+
+    targets: pd.Series = None
+    actuals_yoy: pd.Series = None
+   
     metric_type: str = 'additive'
 
+    def build_cumulative_tracking(self):
 
+        fig = go.Figure()
 
-    def build_cumulative_tracking(df, metric_name, segment_type='overall', chart_type='line', value_type=''):
-    
-    actuals = select_data(df, metric_name, segment_type)
-    targets = select_data(datasets['0 - Targets'].copy(), metric_name, segment_type)
-    
-    fig1 = go.Figure()
+        actuals_ytd = self.actuals.cumsum()
 
-    fig1.add_trace(go.Scatter({
-        'mode': 'lines',
-        'x': targets['target_period_start'],
-        'y': targets['ytd_target'],
-        'name': 'YTD Targets',
-        'line': {
-            'color': '#ffaa80',
-        },
-        'hoverinfo': 'name+x+y+text',
-        }))
-    
-  fig1.add_trace(go.Scatter({
-      'mode': 'lines',
-      'x': actuals['snap_date'],
-      'y': actuals['ytd_value'],
-      'line': {
-        'color': '#2d862d',
-      },
-      'name': 'YTD Actuals',
-      'hoverinfo': 'name+x+y+text',
-    }))
-  
-  fig2 = go.Figure()
-  
-  if chart_type == 'line':
-    fig2.add_trace(go.Scatter({
-        'mode': 'lines',
-        'x': actuals['snap_date'],
-        'y': actuals['yoy_value'],
-        'name': 'Daily YOY',
-        'hoverinfo': 'name+x+y+text',
-        'line': {
-          'color': '#80ccff',
-        },
-      }))
+        fig.add_trace(go.Scatter({
+            'mode': 'lines',
+            'x': self.date_range,
+            'y': actuals_ytd,
+            'line': {
+                'color': '#2d862d',
+            },
+            'name': 'YTD Actuals',
+            'hoverinfo': 'name+x+y+text',
+            }))
+
+        if self.targets is not None:
+            targets_ytd = self.targets.cumsum()
+
+            fig.add_trace(go.Scatter({
+                'mode': 'lines',
+                'x': self.date_range,
+                'y': targets_ytd,
+                'name': 'YTD Targets',
+                'line': {
+                    'color': '#ffaa80',
+                },
+                'hoverinfo': 'name+x+y+text',
+                }))
+        
+        return fig
+
+    def build_yoy_tracking(self, chart_type: str = 'line'):
+        
+        fig = go.Figure()
+
+        if chart_type == 'line':
+
+            fig.add_trace(go.Scatter({
+                'mode': 'lines',
+                'x': self.date_range,
+                'y': self.actuals_yoy,
+                'name': 'Daily YOY',
+                'hoverinfo': 'name+x+y+text',
+                'line': {
+                'color': '#80ccff',
+                },
+            }))
       
-    fig2.add_trace(go.Scatter({
-        'mode': 'lines',
-        'x': actuals['snap_date'],
-        'y': actuals['value'],
-        'name': 'Daily Actuals',
-        'line': {
-          'color': '#005c99',
-        },
-        'hoverinfo': 'name+x+y+text',
-      }))
-  else:
-    fig2.add_trace(go.Bar({
-        'x': actuals['snap_date'],
-        'y': actuals['yoy_value'],
-        'name': 'Daily YOY',
-        'marker_color': '#80ccff',
-        'hoverinfo': 'name+x+y+text',
-      }))
-      
-    fig2.add_trace(go.Bar({
-        'x': actuals['snap_date'],
-        'y': actuals['value'],
-        'name': 'Daily Actuals',
-        'marker_color': '#005c99',
-        'hoverinfo': 'name+x+y+text',
-      }))
-    fig2.update_traces(marker_line_width=1, opacity=0.7)
+            fig.add_trace(go.Scatter({
+                'mode': 'lines',
+                'x': self.date_range,
+                'y': self.actuals,
+                'name': 'Daily Actuals',
+                'line': {
+                'color': '#005c99',
+                },
+                'hoverinfo': 'name+x+y+text',
+            }))
+        else:
+            
+            fig.add_trace(go.Bar({
+                'x': self.date_range,
+                'y': self.actuals_yoy,
+                'name': 'Daily YOY',
+                'marker_color': '#80ccff',
+                'hoverinfo': 'name+x+y+text',
+            }))
+            
+            fig.add_trace(go.Bar({
+                'x': self.date_range,
+                'y': self.actuals,
+                'name': 'Daily Actuals',
+                'marker_color': '#005c99',
+                'hoverinfo': 'name+x+y+text',
+            }))
     
-      
-  for i in range(len(fig1.data)):
-    fig1.data[i].xaxis='x1'
-    fig1.data[i].yaxis='y1'
+            fig2.update_traces(marker_line_width=1, opacity=0.7)
+        
+        return fig
 
-  fig1.layout.xaxis1.update({'anchor': 'y1'})
-  fig1.layout.yaxis1.update({'anchor': 'x1', 'domain': [.55, 1]})
+# %%
+if __name__ == "__name__":
+    import pandas_gbq as pbq
+    project_id = "shopify-data-bigquery-global"
 
-  for i in range(len(fig2.data)):
-      fig2.data[i].xaxis='x2'
-      fig2.data[i].yaxis='y2'
+    def test_query():
+        return '''select * from scratch.practical_dashboards_data
+        where metric = 'gmv' and segment = 'overall' and region = 'Canada'
+        order by snap_date asc
+        '''
 
-  # initialize xaxis2 and yaxis2
-  fig2['layout']['xaxis2'] = {}
-  fig2['layout']['yaxis2'] = {}
+    df = pbq.read_gbq(test_query(), project_id=project_id)
 
-  fig2.layout.xaxis2.update({'anchor': 'y2'})
-  fig2.layout.yaxis2.update({'anchor': 'x2', 'domain': [0, .45]})
+    BuildTargetPlots(actuals=df.value, date_range=df.index, targets=df.target).build_cumulative_tracking()
+    
+    BuildTargetPlots(actuals=df.value, date_range=df.index, actuals_yoy=df.value_yoy).build_yoy_tracking()
 
-  sub_plot_layout = go.Layout({
-    'showlegend': True,
-    'height': 320,
-    'width': 650,
-    'title': {
-      'text': metric_name.upper() + ' - ' + segment_type.upper(),
-      'font': {"size": 14},
-      'x':0.5,
-      'y':0.99,
-      'xanchor': 'center',
-      'yanchor': 'top',
-    },
-    'xaxis': {
-      'title_font': {"size": 10},
-      'tickfont': {"size": 10},
-    },
-    'yaxis': {
-      'tickmode': 'auto',
-      #'title': metric_name.upper(),
-      'ticksuffix': value_type,
-      'title_font': {"size": 10},
-      'tickfont': {"size": 10},
-    },
-    'xaxis2': {
-      'title': 'Date',
-      'title_font': {"size": 10},
-      'tickfont': {"size": 10},
-    },
-    'yaxis2': {
-      'tickmode': 'auto',
-      #'title': metric_name.upper(),
-      'ticksuffix': value_type,
-      'title_font': {"size": 10},
-      'tickfont': {"size": 10},
-    },
-    'legend': {
-      'orientation': 'h',
-      #'xanchor': 'right',
-      #'yanchor': 'top',
-      'bgcolor': 'rgba(0, 0, 0, 0)',
-      'x':0.03,
-      'y':0.99,
-      'traceorder':'normal',
-      'font' : {
-          'size':8,
-       },
-    },
-    'autosize': True,
-    'margin': {'t':20, 'l':0, 'b':0,'r':0},
-    'paper_bgcolor': '#FFF',
-    'plot_bgcolor': 'rgba(0, 0, 0, 0)',
-  })
-  
-  fig = go.Figure(layout=sub_plot_layout)
-  fig.add_traces([fig1.data[0], fig1.data[1],fig2.data[0], fig2.data[1]])
-  fig.layout.update(fig1.layout)
-  fig.layout.update(fig2.layout)
-  return fig
+    MetricEvaluationPipeline(
+        s=df['value'],
+        ).display_actionability_time_series(
+        title='Canada GMV',
+        return_html=False
+    )
+# %%
