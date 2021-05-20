30552b01002d2d275f3ff4f15032ac324a384d7e
Domenic Ranallo <domenic.ranallo@shopify.com>
2021-05-20 11:40:17 -0400

    Annotate And Snooze Metric Check (#13)
    * hackdays files
    
    * updating files
    
    * fixes breaking changes from sprint development
    
    Co-authored-by: Joel Becker <joel.becker@shopify.com>

---
+++ mode_notebook_assets/practical_dashboard_displays/metric_evaluation_pipeline/metric_checks/annotate_and_snooze_metric_check.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+import pandas as pd
+from mode_notebook_assets.practical_dashboard_displays.helper_functions import normalize_valence_score, \
+    map_score_to_string, map_sign_to_string
+from mode_notebook_assets.practical_dashboard_displays.metric_evaluation_pipeline.metric_checks.abstract_metric_check \
+    import AbstractMetricCheck
+from mode_notebook_assets.practical_dashboard_displays.metric_evaluation_pipeline.valence_score import ValenceScore, \
+    ValenceScoreSeries
+
+
+@dataclass
+class AnnotateAndSnoozeMetricCheck(AbstractMetricCheck):
+    """
+    Override the metric annotations to manually specified descriptions.
+    """
+    def apply(self, annotations: pd.Series) -> ValenceScoreSeries:
+
+        def map_annotation_to_result(x: str) -> ValenceScore:
+
+            if x == '':
+                _raw_score = 0
+                _is_override = False
+                _description = x
+                _valence_label = 'Annotation'
+            else:
+                _raw_score = 1
+                _is_override = True
+                _description = x
+                _valence_label = 'Annotation'
+
+            return ValenceScore(
+                valence_score=_raw_score,
+                valence_label=_valence_label,
+                is_override=_is_override,
+                valence_description=_description,
+                metric_check_label='Annotate And Snooze Metric Check',
+            )
+
+        _output = annotations.apply(
+            map_annotation_to_result
+        )
+
+        self._validate_output(s=annotations, _output=_output)
+
+        return ValenceScoreSeries(_output)
