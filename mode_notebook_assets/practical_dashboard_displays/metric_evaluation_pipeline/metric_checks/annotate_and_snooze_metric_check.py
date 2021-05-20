ecfb22f42909ff4fda893a0646c9947f3e9f46ff
Joel Becker <joel.becker@shopify.com>
2021-05-20 11:38:12 -0400

    fixes breaking changes from sprint development

--- mode_notebook_assets/practical_dashboard_displays/metric_evaluation_pipeline/metric_checks/annotate_and_snooze_metric_check.py
+++ mode_notebook_assets/practical_dashboard_displays/metric_evaluation_pipeline/metric_checks/annotate_and_snooze_metric_check.py
@@ -2,10 +2,10 @@ from dataclasses import dataclass
 import pandas as pd
 from mode_notebook_assets.practical_dashboard_displays.helper_functions import normalize_valence_score, \
     map_score_to_string, map_sign_to_string
-from mode_notebook_assets.practical_dashboard_displays.metric_evaluation_pipeline.metric_check_results \
-    import MetricCheckResult
 from mode_notebook_assets.practical_dashboard_displays.metric_evaluation_pipeline.metric_checks.abstract_metric_check \
     import AbstractMetricCheck
+from mode_notebook_assets.practical_dashboard_displays.metric_evaluation_pipeline.valence_score import ValenceScore, \
+    ValenceScoreSeries
 
 
 @dataclass
@@ -13,9 +13,9 @@ class AnnotateAndSnoozeMetricCheck(AbstractMetricCheck):
     """
     Override the metric annotations to manually specified descriptions.
     """
-    def run(self, annotations: pd.Series) -> pd.Series:
+    def apply(self, annotations: pd.Series) -> ValenceScoreSeries:
 
-        def map_annotation_to_result(x: str) -> MetricCheckResult:
+        def map_annotation_to_result(x: str) -> ValenceScore:
 
             if x == '':
                 _raw_score = 0
@@ -28,7 +28,7 @@ class AnnotateAndSnoozeMetricCheck(AbstractMetricCheck):
                 _description = x
                 _valence_label = 'Annotation'
 
-            return MetricCheckResult(
+            return ValenceScore(
                 valence_score=_raw_score,
                 valence_label=_valence_label,
                 is_override=_is_override,
@@ -36,14 +36,10 @@ class AnnotateAndSnoozeMetricCheck(AbstractMetricCheck):
                 metric_check_label='Annotate And Snooze Metric Check',
             )
 
-        if len(annotations.index) == 0:
-            return None
-
-
         _output = annotations.apply(
             map_annotation_to_result
         )
 
         self._validate_output(s=annotations, _output=_output)
 
-        return _output
+        return ValenceScoreSeries(_output)
--- test/practical_dashboard_displays/metric_evaluation_pipeline/metric_checks/test_annotate_and_snooze_metric_check.py
+++ test/practical_dashboard_displays/metric_evaluation_pipeline/metric_checks/test_annotate_and_snooze_metric_check.py
@@ -3,41 +3,43 @@ from itertools import permutations
 import pytest
 import pandas as pd
 
-from mode_notebook_assets.practical_dashboard_displays.metric_evaluation_pipeline.metric_check_results import \
-    MetricCheckResult
+from mode_notebook_assets.practical_dashboard_displays.metric_evaluation_pipeline.valence_score import \
+    ValenceScore, ValenceScoreSeries
 from mode_notebook_assets.practical_dashboard_displays.metric_evaluation_pipeline.metric_checks.annotate_and_snooze_metric_check import \
     AnnotateAndSnoozeMetricCheck
 
-def test_annotations():
-    expected = pd.Series([
-        MetricCheckResult(
-            valence_score=1,
-            is_override=True,
-            valence_label='Annotation',
-            valence_description='Very low due to X',
-            metric_check_label='Annotate And Snooze Metric Check',
-        ),
-        MetricCheckResult(
-            valence_score=0,
-            valence_label='Annotation',
-            valence_description='',
-            metric_check_label='Annotate And Snooze Metric Check',
-        ),
-        MetricCheckResult(
-            valence_score=0,
-            valence_label='Annotation',
-            valence_description='',
-            metric_check_label='Annotate And Snooze Metric Check',
-        ),
-        MetricCheckResult(
-            valence_score=1,
-            is_override=True,
-            valence_label='Annotation',
-            valence_description='Very high due to X',
-            metric_check_label='Annotate And Snooze Metric Check',
-        ),
-    ])
-    actual = AnnotateAndSnoozeMetricCheck().run(pd.Series(['Very low due to X', '', '', 'Very high due to X']))
-    for i in range(0, len(actual)):
-        assert actual[i] == expected[i]
 
+def test_annotations():
+    expected = ValenceScoreSeries(
+        pd.Series([
+            ValenceScore(
+                valence_score=1,
+                is_override=True,
+                valence_label='Annotation',
+                valence_description='Very low due to X',
+                metric_check_label='Annotate And Snooze Metric Check',
+            ),
+            ValenceScore(
+                valence_score=0,
+                valence_label='Annotation',
+                valence_description='',
+                metric_check_label='Annotate And Snooze Metric Check',
+            ),
+            ValenceScore(
+                valence_score=0,
+                valence_label='Annotation',
+                valence_description='',
+                metric_check_label='Annotate And Snooze Metric Check',
+            ),
+            ValenceScore(
+                valence_score=1,
+                is_override=True,
+                valence_label='Annotation',
+                valence_description='Very high due to X',
+                metric_check_label='Annotate And Snooze Metric Check',
+            ),
+        ])
+    )
+    actual = AnnotateAndSnoozeMetricCheck().apply(pd.Series(['Very low due to X', '', '', 'Very high due to X']))
+    for i in range(0, len(actual._score_series)):
+        assert actual._score_series[i] == expected._score_series[i]
