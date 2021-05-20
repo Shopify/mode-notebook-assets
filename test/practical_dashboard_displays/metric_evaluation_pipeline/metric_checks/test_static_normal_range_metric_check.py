54ed6db62337a287d9c31bf8e65a8807d95fb5e3
Joel Becker <joel.becker@shopify.com>
2021-05-20 09:57:04 -0400

    finishes normal range check tests

--- test/practical_dashboard_displays/metric_evaluation_pipeline/metric_checks/test_static_normal_range_metric_check.py
+++ test/practical_dashboard_displays/metric_evaluation_pipeline/metric_checks/test_static_normal_range_metric_check.py
@@ -13,15 +13,19 @@ TEST_CONFIGURATION = StaticNormalRangeMetricCheck(
 )
 
 
-def test_init_manual_four_threshold_metric_check():
+def test_static_normal_range_metric_check_initializes():
     assert TEST_CONFIGURATION, 'Object should initialize.'
 
 
-def test_run_check():
+def test_static_normal_range_metric_check_apply_finishes():
     assert isinstance(TEST_CONFIGURATION.apply(pd.Series([25, 25, 5, 15, 35, 45])), ValenceScoreSeries), \
-        'Run method should finish executing.'
+        'Apply method should finish executing.'
 
+
+def test_static_normal_range_metric_check_example():
     _output = StaticNormalRangeMetricCheck(
-        rolling_periods=4,
+        rolling_periods=8,
         maximum_learning_differences_quantile=.8,
     ).apply(pd.Series([250, 190, 230, 210, 220, 220, 190, 225, 220, 225, 300, 240, 100, 500, 0]))
+
+    assert [v.valence_score for v in _output._score_series] == [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, -1, 1, -1]
